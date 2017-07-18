5f6da2719ac63829691bcdc27cd4f4c3c5b4bb99
Catherine Tarsney <cjtarsney@gmail.com>
2017-07-17 23:02:29 -0500

    app header

---
+++ index.js
@@ -0,0 +1,20 @@
+var express             = require('express'),
+    connect             = require('connect'),
+    url                 = require('url'),
+    http                = require('http'),
+    async               = require('async'),
+    webSocketServer     = require('ws').Server;
+    require('dotenv').config();
+
+var app = express(),
+    server = http.createServer(app),
+    port = process.env.PORT || 5000;
+
+server.listen(port);
+console.log("http server listening on %d", port);
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/',function(req,res){
+  res.sendFile(__dirname+'/public/home.html');
+});
