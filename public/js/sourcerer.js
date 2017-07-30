16bb7fd3b04822c96a649217b56c10a277fca521
Catherine Tarsney <cjtarsney@gmail.com>
2017-07-30 16:22:47 -0500

    account setup and login functionality

--- public/js/sourcerer.js
+++ public/js/sourcerer.js
@@ -1,5 +1,24 @@
 var state = {}
 
+var loggedInDropdown = [
+    {label:"Settings"
+      , class:"settings"
+      , id:"settings"
+      , "data-toggle":"modal"
+      , "data-target":"#account-settings-modal"},
+    {label:"Logout"
+      , class:"settings"
+      , id:"user-logout"
+      , "data-toggle":"modal"
+      , "data-target":"#logged-out-modal"}]
+
+  var loggedOutDropdown = [
+      {label:"Log In"
+        , class:"settings"
+        , id:"login"
+        , "data-toggle":"modal"
+        , "data-target":"#login-modal"}]
+
 var content = function(contentParams){
   var order=['date','sender','subject','audience']
   contentParams.date=contentParams.date.replace(/-/g,"")
@@ -54,6 +73,16 @@ var paramFunctions = {
 
 $(document).ready(function(){
 
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      login();
+    } else {
+      console.log("User is logged out");
+    }
+  });
+
+
+
   var updateURL = function(){
 
     var params={};
@@ -64,6 +93,7 @@ $(document).ready(function(){
     }).get();
 
     var unsourcedURL = $("#basic-url").val();
+
 
     $(".input-group>.param:not(.content-form)").map(function(idx,elem){
       var id = $(elem).attr("id");
@@ -89,13 +119,14 @@ state.tools=activeToolNames;
 console.log(state.tools);
 
 if(unsourcedURL.includes("?")){
-return ("http://"+(unsourcedURL+"&").replace(/\/\//g, '/')).replace(/http:\/\/https:\//g,"https://").replace(/http:\/\/http:\//g,"http://") +tools.map(function(d){
+return ($("#protocol-display>a").text()+(unsourcedURL+"&").replace(/\/\//g, '/')).replace(/http:\/\/https:\//g,"https://").replace(/http:\/\/http:\//g,"http://").replace(/https:\/\/https:\//g,"https://").replace(/https:\/\/http:\//g,"http://") +tools.map(function(d){
   return paramFunctions[d](params)
 }).join("&").replace(/ /g,"-");
 } else {
-  return ("http://"+(unsourcedURL+"/?").replace(/\/\//g, '/')).replace(/http:\/\/https:\//g,"https://").replace(/http:\/\/http:\//g,"http://") +tools.map(function(d){
+  return ($("#protocol-display>a").text()+(unsourcedURL+"/?").replace(/\/\//g, '/')).replace(/http:\/\/https:\//g,"https://").replace(/http:\/\/http:\//g,"http://").replace(/https:\/\/https:\//g,"https://").replace(/https:\/\/http:\//g,"http://") +tools.map(function(d){
     return paramFunctions[d](params)
   }).join("&").replace(/ /g,"-");
+
 }
 
 
@@ -125,7 +156,7 @@ return ("http://"+(unsourcedURL+"&").replace(/\/\//g, '/')).replace(/http:\/\/ht
   })
 
 $("#test").on("click",function(){
-  window.open($("#url-holder").val(),"_blank")
+  window.open($("#url-holder").val(),"_blank");
   ga('send', 'event', 'test', 'click', 'URL Tested');
 })
 
@@ -142,6 +173,137 @@ $(".dropdown-menu>li").on("click",function(idx,elem){
   $(".param#"+id).val(value);
   $("#url-holder").text(updateURL());
   $("#url-holder").val(updateURL());
+})
+
+$(".protocol-choice>li").on("click",function(idx,elem){
+  var protocol = $(this).attr("value");
+  console.log(protocol);
+  if (protocol=="https://"){
+
+    $("#protocol-display>a").text("https://");
+    $(".protocol-choice>li").attr("value","http://");
+    $(".protocol-choice>li>a").text("http://");
+}
+  else {
+    $("#protocol-display>a").text("http://");
+    $(".protocol-choice>li").attr("value","https://");
+    $(".protocol-choice>li>a").text("https://");
+
+}
+  var protocol = $(".protocol-choice>li").attr("value");
+  console.log(protocol);
+  $("#url-holder").text(updateURL());
+  $("#url-holder").val(updateURL());
+;
+})
+
+$("#signup-link>a").on("click", function(){
+  $("#login-modal").modal("hide");
+})
+
+$("#login-link>a").on("click", function(){
+  $("#signup-modal").modal("hide");
+})
+
+$("#new-password-confirm").on("input",function(d){
+  console.log("password entered")
+  if($("#new-password-confirm").val()===$("#new-password").val()){
+          $("#passwords-match").attr("class", "validate fa fa-check");
+          $("#passwords-mismatch").attr("class", "hidden validate fa fa-times");
+          $("#create-account").attr("disabled",null);
+        }else{
+          $("#passwords-match").attr("class", "hidden validate fa fa-check");
+          $("#passwords-mismatch").attr("class", "validate fa fa-times");
+          $("#create-account").attr("disabled","true");
+        }
+
+})
+
+var login = function(){
+  $("#login-modal").modal('hide');
+  $("#signup-modal").modal('hide');
+  $("#hamburger-menu>li").remove();
+  loggedInDropdown.map(function(d){
+    $("<li><a></a></li>")
+                      .find("a")
+                      .attr("class",d.class)
+                      .attr("id",d.id)
+                      .attr("value",d.label)
+                      .attr("data-toggle",d["data-toggle"])
+                      .attr("data-target",d["data-target"])
+                      .text(d.label)
+                      .end()
+                      .appendTo($("#hamburger-menu"))
+  });
+  $("#user-logout").on("click",function(){
+    console.log("step one");
+    firebase.auth().signOut().then(function() {
+      console.log("step two");
+    logout()
+    }).catch(function(error) {
+    // An error happened.
+  });
+
+  })
+}
+
+var logout = function(){
+  $("#hamburger-menu>li").remove();
+  loggedOutDropdown.map(function(d){
+    $("<li><a></a></li>")
+                      .find("a")
+                      .attr("class",d.class)
+                      .attr("id",d.id)
+                      .attr("value",d.label)
+                      .attr("data-toggle",d["data-toggle"])
+                      .attr("data-target",d["data-target"])
+                      .text(d.label)
+                      .end()
+                      .appendTo($("#hamburger-menu"))
+  })
+}
+
+
+
+$("#create-account").on("click",function(){
+  console.log("test");
+  var email = $("#new-username").val()
+  var password = $("#new-password").val()
+  console.log(email);
+  firebase.auth().createUserWithEmailAndPassword(email, password).then(
+    function(user){
+      firebase.database().ref('users/' + user.uid).set({
+        email: email
+      }).then(function(){
+        login()
+      })
+    }
+  ).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // ...
+});
+
+})
+
+$("#user-login").on("click",function(){
+  var email = $("#username").val()
+  var password = $("#password").val()
+  firebase.auth().signInWithEmailAndPassword(email, password).then(
+    function(user){
+      firebase.database().ref('users/' + user.uid).set({
+        email: email
+      }).then(function(){
+        login()
+      })
+    }
+  ).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ...
+  });
 })
 
 
